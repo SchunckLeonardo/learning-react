2109c702d3cf497471982ba175c48886adb218db
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-02-26 17:09:32 -0300

    State com Arrays

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "The title 1",
+        body: "The body 1"
+      },
+      {
+        id: 2,
+        title: "The title 2",
+        body: "The body 2"
+      },
+      {
+        id: 13,
+        title: "The title 3",
+        body: "The body 3"
+      },
+    ]
+  }
+
+  render() {
+    const { posts } = this.state
+
+    return (
+      <>
+        <div className="App">
+          {posts.map(post => (
+            <div key={post.id}>
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          ))}
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;
