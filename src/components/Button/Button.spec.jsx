a791008b7ffb2bd01ae095fa71365203f8f93aac
Leonardo Schunck Rainha <schunckrainhaleonardo@gmail.com>
2024-03-04 19:55:39 -0300

    Setup Tests to Button

---
+++ src/components/Button/Button.spec.jsx
@@ -0,0 +1,50 @@
+import { render, screen } from "@testing-library/react"
+import Button from "."
+import userEvent from "@testing-library/user-event"
+
+describe('<Button />', () => {
+
+    it('should render the button with the text "Load More"', () => {
+        render(<Button text="Load more" />)
+
+        const button = screen.getByRole('button', {
+            name: /load more/i
+        })
+        expect(button).toBeInTheDocument()
+    })
+
+    it('should call function on button click', () => {
+        const fn = jest.fn()
+        render(<Button text="Load more" event={fn} />)
+
+
+        const button = screen.getByRole('button', {
+            name: /load more/i
+        })
+
+        userEvent.click(button)
+
+        expect(fn).toHaveBeenCalled()
+    })
+
+    it('should be disabled if disabled is true"', () => {
+        render(<Button text="Load more" disabled={true} />)
+
+        const button = screen.getByRole('button', {
+            name: /load more/i
+        })
+
+        expect(button).toBeDisabled()
+    })
+
+    it('should be enabled if disabled is false"', () => {
+        render(<Button text="Load more" disabled={false} />)
+
+        const button = screen.getByRole('button', {
+            name: /load more/i
+        })
+
+        expect(button).toBeEnabled()
+    })
+
+})
---
+++ src/template/Home/Home.spec.jsx
@@ -0,0 +1,7 @@
+describe("<Home />", () => {
+
+    it('test 1', () => {
+        expect(2).toBe(2)
+    })
+
+})
